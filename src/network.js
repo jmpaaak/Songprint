4dbd0920699f7eab3b106abd9e2c036f4f13354c
park jongmin <jmpaaak@gmail.com>
2016-06-23 05:51:57 +0900

    network completed

--- src/network.js
+++ src/network.js
@@ -1,160 +1,128 @@
 var w = window.innerWidth || document.body.clientWidth,
     h = window.innerHeight || document.body.clientHeight;
-var padding = 80;
-var graphW = w-padding * 2;
-var graphH = h-padding * 2;
-var nodeSize = graphW/15;
+var paddingX = 80, paddingY = 10;
+var graphW = w-paddingX * 2;
+var graphH = h-paddingY * 2;
+var nodeSize = graphW/40;
+var LinkData = [];
+var ToneData = [];
 
-var svg = d3.select("#container").attr("align","center")
+var svg = d3.select("#container")
+    .attr("align","center")
+    .style("padding-top", paddingY+"px")
+    .style("padding-bottom", paddingY+"px")
+    .style("padding-right", paddingX+"px")
+    .style("padding-left", paddingX+"px")
     .append("svg")
     .attr("width", graphW)
-    .attr("height", h);
+    .attr("height", graphH);
 
 var force = d3.layout.force()
-    .gravity(0)
-    .distance(100)
-    .charge(0)
+    .gravity(.07)
+    .distance(30)
+    .charge(-70)
     .size([graphW, graphH]);
 
-d3.json("graphFile.json", function (json) {
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
+//init data
+d3.json("ToneData.json", function (json) {
+    ToneData = json;
+    force.nodes(ToneData);
 
-    var link = svg.selectAll(".link")
-        .data(json.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-            return Math.sqrt(d.weight);
-        });
+    //parsing data Sting to Float
+    ToneData = ToneData.map(function(elem) {
+        var attr_arr = Object.getOwnPropertyNames(elem);
+        var song_name = attr_arr[0];
+        for(var i=0; i<elem[song_name].length; i++) {
+            var d_obj = elem[song_name][i];
+            d_obj["value"] = parseFloat(d_obj["value"]-7.5);
+            elem[song_name][i] = d_obj;
+        }
+        return elem;
+    });
 
-    var nodes = svg.append("g")
-        .attr("class", "nodes")
-        .attr("transform", "translate(-" + nodeSize/2 + ", -" + nodeSize/2 + ")");
+    d3.json("graphFile.json", function (json) {
+        LinkData = json.links;
+        force.links(LinkData);
+        force.start();
 
-    var node = nodes.selectAll(".node")
-        .data(json.nodes)
-        .enter().append("g")
-        .attr("class", "node")
-        .attr("id", function(d) {
-            return d.name
-        })
-        .call(force.drag)
-        .each(function() {
-            d3.select(this).call(drawRadarChart);
-        });
-
-    // node.append("text")
-    //     .attr("dx", 12)
-    //     .attr("dy", ".35em")
-    //     .text(function (d) {
-    //         return d.name
-    //     });
-
-    force.on("tick", function () {
-        link.attr("x1", function (d) {
-            return d.source.x;
-        })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
+        var link = svg.selectAll(".link")
+            .data(LinkData)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function (d) {
+                return Math.sqrt(d.weight);
             });
 
-        node.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
+        var nodes = svg.append("g")
+            .attr("class", "nodes")
+            .attr("transform", "translate(-" + nodeSize/2 + ", -" + nodeSize/2 + ")");
+
+        var node = nodes.selectAll(".node")
+            .data(ToneData)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("id", function(d) {
+                return "d"+d.index
+            })
+            .call(force.drag)
+            .each(function() {
+                d3.select(this).call(drawRadarChart);
+            });
+
+        node.append("text")
+            .attr("dx", 0)
+            .attr("dy", 4)
+            .text(function (d) {
+                var propArr = Object.getOwnPropertyNames(d);
+                return propArr[0];
+            });
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                return d.source.x;
+            })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node.attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
         });
     });
+
 });
-
-var colorscale = d3.scale.category10();
-
-//Legend titles
-var LegendOptions = ['Smartphone','Tablet'];
-
-//Data
-var d = [
-    [
-        {axis:"Email",value:0.59},
-        {axis:"Social Networks",value:0.56},
-        {axis:"Internet Banking",value:0.42},
-        {axis:"News Sportsites",value:0.34},
-        {axis:"Search Engine",value:0.48},
-        {axis:"View Shopping sites",value:0.14},
-        {axis:"Paying Online",value:0.11},
-        {axis:"Buy Online",value:0.05},
-        {axis:"Stream Music",value:0.07},
-        {axis:"Online Gaming",value:0.12},
-        {axis:"Navigation",value:0.27},
-        {axis:"App connected to TV program",value:0.03},
-        {axis:"Offline Gaming",value:0.12},
-        {axis:"Photo Video",value:0.4},
-        {axis:"Reading",value:0.03},
-        {axis:"Listen Music",value:0.22},
-        {axis:"Watch TV",value:0.03},
-        {axis:"TV Movies Streaming",value:0.03},
-        {axis:"Listen Radio",value:0.07},
-        {axis:"Sending Money",value:0.18},
-        {axis:"Other",value:0.07},
-        {axis:"Use less Once week",value:0.08}
-    ],[
-        {axis:"Email",value:0.48},
-        {axis:"Social Networks",value:0.41},
-        {axis:"Internet Banking",value:0.27},
-        {axis:"News Sportsites",value:0.28},
-        {axis:"Search Engine",value:0.46},
-        {axis:"View Shopping sites",value:0.29},
-        {axis:"Paying Online",value:0.11},
-        {axis:"Buy Online",value:0.14},
-        {axis:"Stream Music",value:0.05},
-        {axis:"Online Gaming",value:0.19},
-        {axis:"Navigation",value:0.14},
-        {axis:"App connected to TV program",value:0.06},
-        {axis:"Offline Gaming",value:0.24},
-        {axis:"Photo Video",value:0.17},
-        {axis:"Reading",value:0.15},
-        {axis:"Listen Music",value:0.12},
-        {axis:"Watch TV",value:0.1},
-        {axis:"TV Movies Streaming",value:0.14},
-        {axis:"Listen Radio",value:0.06},
-        {axis:"Sending Money",value:0.16},
-        {axis:"Other",value:0.07},
-        {axis:"Use less Once week",value:0.17}
-    ]
-];
-
 
 //Options for the Radar chart, other than default
 var mycfg = {
     w: nodeSize,
     h: nodeSize,
-    maxValue: 0.6,
-    levels: 6,
-    ExtraWidthX: 0,
+    maxValue: 36.5-7.5,
+    levels: 0,
     radius: 1,
-    factorLegend: .85,
-    maxValue: 0,
+    factor: 1,
+    factorLegend: 1,
     radians: 2 * Math.PI,
     TranslateX: 0,
     TranslateY: 0,
     ExtraWidthX: 0,
     ExtraWidthY: 0
 };
-/*
-//Call function to draw the Radar chart
-//Will expect that data is in %'s
-RadarChart.draw("#chart", d, mycfg);
-*/
 
 function drawRadarChart() {
-    var id = this.attr("id");
-    RadarChart.draw("#"+id, d, mycfg);
+    var id_str = this.attr("id");
+    var id_str_split = id_str.split('');
+    id_str_split.shift();
+    var id = id_str_split.join('');
+    var attr_arr = Object.getOwnPropertyNames(ToneData[id]);
+    var song_name = attr_arr[0];
+    RadarChart.draw("#d"+id, new Array(ToneData[id][song_name]), mycfg);
 }
 
 ////////////////////////////////////////////
